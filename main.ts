8055fd3991faceebf427c4dbe6b5e7a2042ac44b
DaneMaldonado <dmaldonado2025@communityschoolnaples.org>
2025-05-02 08:44:44 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,4 @@
-function movement (character: Sprite) {
-    controller.moveSprite(character, 150, 0)
-    // Only allow horizontal movement
-    character.ay = 150
-    // Gravity strength
-    character.setFlag(SpriteFlag.StayInScreen, true)
-    controller.A.onEvent(ControllerButtonEvent.Pressed, function on_jump_pressed() {
-        if (character.isHittingTile(CollisionDirection.Bottom)) {
-            character.vy = -150
-        }
-        
-    })
-}
-function SpawnExplorer () {
+function spawn_explorer () {
     for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
         Explorer = sprites.create(img`
             . . . . . f f 4 4 f f . . . . . 
@@ -35,6 +22,19 @@ function SpawnExplorer () {
         scene.cameraFollowSprite(Explorer)
     }
 }
+function movement (character: Sprite) {
+    controller.moveSprite(character, 150, 0)
+    // Only allow horizontal movement
+    character.ay = 150
+    // Gravity strength
+    character.setFlag(SpriteFlag.StayInScreen, true)
+    controller.A.onEvent(ControllerButtonEvent.Pressed, function on_jump_pressed() {
+        if (character.isHittingTile(CollisionDirection.Bottom)) {
+            character.vy = -150
+        }
+        
+    })
+}
 function spawn_mermaid () {
     for (let value2 of tiles.getTilesByType(assets.tile`myTile`)) {
         Mermaid = sprites.create(img`
@@ -55,32 +55,276 @@ function spawn_mermaid () {
             . . . f f f f f f f f f f . . . 
             . . . . . f f . . f f . . . . . 
             `, SpriteKind.Player)
-        tiles.placeOnTile(Explorer, value2)
-        scene.cameraFollowSprite(Explorer)
+        tiles.placeOnTile(Mermaid, value2)
+        scene.cameraFollowSprite(Mermaid)
     }
 }
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    Shark,
+    [img`
+        .............ccfff..............
+        ...........ccddbcf..............
+        ..........ccddbbf...............
+        ..........fccbbcf...............
+        .....fffffccccccff.........ccc..
+        ...ffbbbbbbbcbbbbcfff....ccbbc..
+        ..fbbbbbbbbcbcbbbbcccff.cdbbc...
+        ffbbbbbbffbbcbcbbbcccccfcdbbf...
+        fbcbbb11ff1bcbbbbbcccccffbbf....
+        fbbb11111111bbbbbcccccccbbcf....
+        .fb11133cc11bbbbcccccccccccf....
+        ..fccc31c111bbbcccccbdbffbbcf...
+        ...fc13c111cbbbfcddddcc..fbbf...
+        ....fccc111fbdbbccdcc.....fbbf..
+        ........ccccfcdbbcc........fff..
+        .............fffff..............
+        `],
+    500,
+    false
+    )
+    animation.runImageAnimation(
+    Explorer,
+    [img`
+        . . . f 4 4 f f f f . . . . . . 
+        . . f 4 5 5 4 5 f b f f . . . . 
+        . . f 5 5 5 5 4 e 3 3 b f . . . 
+        . . f e 4 4 4 e 3 3 3 3 b f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e e 3 b e 3 3 3 3 f . . 
+        . f 3 3 e e e f f 3 3 3 3 f . . 
+        . f 3 e e e f b f b b b b f . . 
+        . . f e 4 4 f 1 e b b b b f . . 
+        . . . f 4 4 4 4 f b b b b f f . 
+        . . . f e e e f f f b b b b f . 
+        . . . f d d d e 4 4 f b b f . . 
+        . . . f d d d e 4 4 e f f . . . 
+        . . f b d b d b e e b f . . . . 
+        . . f f 1 d 1 d 1 d f f . . . . 
+        . . . . f f b b f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . f 4 4 f f f f . . . . . . 
+        . . f 4 5 5 4 5 f b f f . . . . 
+        . . f 5 5 5 5 4 e 3 3 b f . . . 
+        . . f e 4 4 4 e 3 3 3 3 b f . . 
+        . f 3 3 3 3 3 3 3 3 3 3 3 f . . 
+        . f 3 3 e e 3 b e 3 3 3 3 f . . 
+        . f 3 3 e e e f f 3 3 3 3 f . . 
+        . . f e e e f b f b b b b f f . 
+        . . . e 4 4 f 1 e b b b b b f . 
+        . . . f 4 4 4 4 f b b b b b f . 
+        . . . f d d d e 4 4 b b b f . . 
+        . . . f d d d e 4 4 f f f . . . 
+        . . f d d d b b e e b b f . . . 
+        . . f b d 1 d 1 d d b f . . . . 
+        . . . f f f b b f f f . . . . . 
+        `],
+    500,
+    false
+    )
+    animation.runImageAnimation(
+    Explorer,
+    [img`
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . f 6 2 5 5 6 f . . . . . 
+        . . . f 6 6 6 6 1 6 6 f . . . . 
+        . . . f 6 6 6 6 6 1 6 f . . . . 
+        . . . f d f d 6 6 6 1 f . . . . 
+        . . . f d f d 6 6 6 6 f f . . . 
+        . . . f d 3 d d 6 6 6 f 6 f . . 
+        . . . . f d d d f f 6 f f . . . 
+        . . . . . f f 5 3 f 6 6 6 f . . 
+        . . . . f 5 3 3 f f f f f . . . 
+        . . . . f 3 3 f d f . . . . . . 
+        . . . . . f 3 f d f . . . . . . 
+        . . . . f 3 5 3 f d f . . . . . 
+        . . . . f f 3 3 f f . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 . 5 . . . . . . 
+        . . . . . . f 5 5 5 f . . . . . 
+        . . . . . f 6 2 5 5 6 f . . . . 
+        . . . . f 6 6 6 6 1 6 6 f . . . 
+        . . . . f 6 6 6 6 6 1 6 f . . . 
+        . . . . f d f d 6 6 6 1 f . . . 
+        . . . . f d f d 6 6 6 6 f f . . 
+        . . . . f d 3 d d 6 6 6 f 6 f . 
+        . . . . . f d d d f f 6 f f . . 
+        . . . . . . f f 3 3 f f 6 6 f . 
+        . . . . . f d d d d f f f f . . 
+        . . . . . f d d d f 3 f . . . . 
+        . . . . . . f f f d 5 3 f . . . 
+        . . . . . f f f 3 3 f f . . . . 
+        . . . . . f f f f f f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 5 . 5 . . . . . . 
+        . . . . . . f 5 5 5 f . . . . . 
+        . . . . . f 6 2 5 5 6 f . . . . 
+        . . . . f 6 6 6 6 1 6 6 f . . . 
+        . . . . f 6 6 6 6 6 1 6 f . . . 
+        . . . . f d f d 6 6 6 1 f . . . 
+        . . . . f d f d 6 6 6 6 f f . . 
+        . . . . f d 3 d d 6 6 6 f 6 f . 
+        . . . . . f d d d f f 6 f f . . 
+        . . . . . . f f 3 3 f f 6 6 f . 
+        . . . . . f 5 3 3 d d f f f . . 
+        . . . . . f 3 3 3 f d d f . . . 
+        . . . . . . f 3 5 f f f . . . . 
+        . . . . . f 3 3 3 3 f . . . . . 
+        . . . . . . f f f f f . . . . . 
+        `],
+    500,
+    false
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    Shark,
+    [img`
+        ..............fffcc.............
+        ..............fcbddcc...........
+        ...............fbbddcc..........
+        ...............fcbbccf..........
+        ..ccc.........ffccccccfffff.....
+        ..cbbcc....fffcbbbbcbbbbbbbff...
+        ...cbbdc.ffcccbbbbcbcbbbbbbbbf..
+        ...fbbdcfcccccbbbcbcbbffbbbbbbff
+        ....fbbffcccccbbbbbcb1ff11bbbcbf
+        ....fcbbcccccccbbbbb11111111bbbf
+        ....fcccccccccccbbbb11cc33111bf.
+        ...fcbbffbdbcccccbbb111c13cccf..
+        ...fbbf..ccddddcfbbbc111c31cf...
+        ..fbbf.....ccdccbbdbf111cccf....
+        ..fff........ccbbdcfcccc........
+        ..............fffff.............
+        `],
+    500,
+    false
+    )
+    animation.runImageAnimation(
+    Explorer,
+    [img`
+        . . . . . . f f f f 4 4 f . . . 
+        . . . . f f b f 5 4 5 5 4 f . . 
+        . . . f b 3 3 e 4 5 5 5 5 f . . 
+        . . f b 3 3 3 3 e 4 4 4 e f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 f . . 
+        . . f 3 3 3 3 e b 3 e e 3 3 f . 
+        . . f 3 3 3 3 f f e e e 3 3 f . 
+        . . f b b b b f b f e e e 3 f . 
+        . . f b b b b e 1 f 4 4 e f . . 
+        . f f b b b b f 4 4 4 4 f . . . 
+        . f b b b b f f f e e e f . . . 
+        . . f b b f 4 4 e d d d f . . . 
+        . . . f f e 4 4 e d d d f . . . 
+        . . . . f b e e b d b d b f . . 
+        . . . . f f d 1 d 1 d 1 f f . . 
+        . . . . . . f f b b f f . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f f 4 4 f . . . 
+        . . . . f f b f 5 4 5 5 4 f . . 
+        . . . f b 3 3 e 4 5 5 5 5 f . . 
+        . . f b 3 3 3 3 e 4 4 4 e f . . 
+        . . f 3 3 3 3 3 3 3 3 3 3 3 f . 
+        . . f 3 3 3 3 e b 3 e e 3 3 f . 
+        . . f 3 3 3 3 f f e e e 3 3 f . 
+        . f f b b b b f b f e e e f . . 
+        . f b b b b b e 1 f 4 4 e . . . 
+        . f b b b b b f 4 4 4 4 f . . . 
+        . . f b b b 4 4 e d d d f . . . 
+        . . . f f f 4 4 e d d d f . . . 
+        . . . f b b e e b b d d d f . . 
+        . . . . f b d d 1 d 1 d b f . . 
+        . . . . . f f f b b f f f . . . 
+        `],
+    500,
+    false
+    )
+    animation.runImageAnimation(
+    Explorer,
+    [img`
+        . . . . . . . 5 . 5 . . . . . . 
+        . . . . . . f 5 5 5 f . . . . . 
+        . . . . . f 6 5 5 2 6 f . . . . 
+        . . . . f 6 6 1 6 6 6 6 f . . . 
+        . . . . f 6 1 6 6 6 6 6 f . . . 
+        . . . . f 1 6 6 6 d f d f . . . 
+        . . . f f 6 6 6 6 d f d f . . . 
+        . . f 6 f 6 6 6 d d 3 d f . . . 
+        . . . f f 6 f f d d d f . . . . 
+        . . f 6 6 6 f 3 5 f f . . . . . 
+        . . . f f f f f 3 3 5 f . . . . 
+        . . . . . . f d f 3 3 f . . . . 
+        . . . . . . f d f 3 f . . . . . 
+        . . . . . f d f 3 5 3 f . . . . 
+        . . . . . . f f 3 3 f f . . . . 
+        . . . . . . . f f f . . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . f 6 5 5 2 6 f . . . . . 
+        . . . f 6 6 1 6 6 6 6 f . . . . 
+        . . . f 6 1 6 6 6 6 6 f . . . . 
+        . . . f 1 6 6 6 d f d f . . . . 
+        . . f f 6 6 6 6 d f d f . . . . 
+        . f 6 f 6 6 6 d d 3 d f . . . . 
+        . . f f 6 f f d d d f . . . . . 
+        . f 6 6 f f 3 3 f f . . . . . . 
+        . . f f f f d d d d f . . . . . 
+        . . . . f 3 f d d d f . . . . . 
+        . . . f 3 5 d f f f . . . . . . 
+        . . . . f f 3 3 f f f . . . . . 
+        . . . . f f f f f f f . . . . . 
+        `,img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . 5 . 5 . . . . . . . 
+        . . . . . f 5 5 5 f . . . . . . 
+        . . . . f 6 5 5 2 6 f . . . . . 
+        . . . f 6 6 1 6 6 6 6 f . . . . 
+        . . . f 6 1 6 6 6 6 6 f . . . . 
+        . . . f 1 6 6 6 d f d f . . . . 
+        . . f f 6 6 6 6 d f d f . . . . 
+        . f 6 f 6 6 6 d d 3 d f . . . . 
+        . . f f 6 f f d d d f . . . . . 
+        . f 6 6 f f 3 3 f f . . . . . . 
+        . . f f f d d 3 3 5 f . . . . . 
+        . . . f d d f 3 3 3 f . . . . . 
+        . . . . f f f 5 3 f . . . . . . 
+        . . . . . f 3 3 3 3 f . . . . . 
+        . . . . . f f f f f . . . . . . 
+        `],
+    500,
+    false
+    )
+})
 function spawn_shark () {
-    for (let value3 of tiles.getTilesByType(assets.tile`myTile`)) {
+    for (let value of tiles.getTilesByType(assets.tile`myTile`)) {
         Shark = sprites.create(img`
-            .............ccfff..............
-            ...........ccddbcf..............
-            ..........ccddbbf...............
-            ..........fccbbcf...............
-            .....fffffccccccff.........ccc..
-            ...ffbbbbbbbcbbbbcfff....ccbbc..
-            ..fbbbbbbbbcbcbbbbcccff.cdbbc...
-            ffbbbbbbffbbcbcbbbcccccfcdbbf...
-            fbcbbb11ff1bcbbbbbcccccffbbf....
-            fbbb11111111bbbbbcccccccbbcf....
-            .fb11133cc11bbbbcccccccccccf....
-            ..fccc31c111bbbcccccbdbffbbcf...
-            ...fc13c111cbbbfcddddcc..fbbf...
-            ....fccc111fbdbbccdcc.....fbbf..
-            ........ccccfcdbbcc........fff..
-            .............fffff..............
+            ..............fffcc.............
+            ..............fcbddcc...........
+            ...............fbbddcc..........
+            ...............fcbbccf..........
+            ..ccc.........ffccccccfffff.....
+            ..cbbcc....fffcbbbbcbbbbbbbff...
+            ...cbbdc.ffcccbbbbcbcbbbbbbbbf..
+            ...fbbdcfcccccbbbcbcbbffbbbbbbff
+            ....fbbffcccccbbbbbcb1ff11bbbcbf
+            ....fcbbcccccccbbbbb11111111bbbf
+            ....fcccccccccccbbbb11cc33111bf.
+            ...fcbbffbdbcccccbbb111c13cccf..
+            ...fbbf..ccddddcfbbbc111c31cf...
+            ..fbbf.....ccdccbbdbf111cccf....
+            ..fff........ccbbdcfcccc........
+            ..............fffff.............
             `, SpriteKind.Player)
-        tiles.placeOnTile(Explorer, value3)
-        scene.cameraFollowSprite(Explorer)
+        tiles.placeOnTile(Shark, value)
+        scene.cameraFollowSprite(Shark)
     }
 }
 let Shark: Sprite = null
@@ -104,7 +348,7 @@ let avatarChoice = game.askForNumber("Perfect " + playerName + `
 // Spawn selected avatar
 if (avatarChoice == 1) {
     selectedAvatar = "Explorer"
-    SpawnExplorer()
+    spawn_explorer()
 } else if (avatarChoice == 2) {
     selectedAvatar = "Mermaid"
     spawn_mermaid()
@@ -135,5 +379,5 @@ if (selectedAvatar == "Explorer") {
 } else if (selectedAvatar == "Mermaid") {
     movement(Mermaid)
 } else if (selectedAvatar == "Shark") {
-    movement(Mermaid)
+    movement(Shark)
 }
